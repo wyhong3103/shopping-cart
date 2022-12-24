6d724575abdf039845b1764529c0dbeff4c474f7
wyhong3103 <wyhong3103@gmail.com>
2022-12-24 15:04:16 +0800

    Finish up home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,12 @@
 const Home = () => {
-
+    return(
+        <div className="home-cont">
+            <div class="home-flex">
+                <h1>GamingWizard</h1>
+                <p>We have everything you need here.</p>
+            </div>
+        </div>
+    )
 }
 
 export default Home;
