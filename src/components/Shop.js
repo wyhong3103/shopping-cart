d87e00b141dff10fcfb41180f4ed6d8796466731
wyhong3103 <wyhong3103@gmail.com>
2022-12-25 20:04:48 +0800

    Complete shop component

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,7 +1,15 @@
-const Shop = () => {
+import {ItemCard} from './ItemCard';
+
+const Shop = ({products}) => {
     return(
         <div className="shop-cont">
-
+            {
+                products.map((item) => {
+                    return(
+                        <ItemCard key={item.id} item={item}/>
+                    )
+                })
+            }
         </div>
     )
 }
