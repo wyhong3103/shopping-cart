368b53a1d2983bf2427aa2708d218ad8644da160
wyhong3103 <wyhong3103@gmail.com>
2022-12-25 23:23:59 +0800

    Complete cart component and cart item component

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,5 +1,29 @@
-const Cart = () => {
+import CartItem from "./CartItem";
 
+const Cart = ({cart}) => {
+    let total = 0;
+    for(const i of cart){
+        total += Number(i.qty) * Number(i.price);
+    }
+
+    return(
+        <div className="cart-cont">
+            <div className="cart-items">
+                {
+                    cart.map(
+                        (item) => {
+                            return(
+                                <CartItem key={item.id} item={item}/>
+                            )
+                        }
+                    )
+                }
+            </div>
+            <div>
+                <p>Total : {total}</p>
+            </div>
+        </div>
+    )
 }
 
 export default Cart;
---
+++ src/components/CartItem.js
@@ -0,0 +1,18 @@
+const CartItem = ({item}) => {
+    const image = require(`../assets/${item.id}.jpg`);
+
+    return(
+        <div className="cart-item">
+            <div className="left">
+                <img src={image} alt={`item-${item.id}`}/>
+            </div>
+            <div className="right">
+                <h3 className="cart-game-title">{item.name}</h3>
+                <p>{item.qty}</p>
+                <p>${Number(item.qty) * Number(item.price)}</p>
+            </div>
+        </div>
+    )
+};
+
+export default CartItem;
