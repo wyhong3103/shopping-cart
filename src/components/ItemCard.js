4883ad3974cd93a8ed3e84128e9cfd67e8ecbe56
wyhong3103 <wyhong3103@gmail.com>
2022-12-25 20:05:09 +0800

    Destructure prop instead of using prop

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -1,12 +1,12 @@
-const ItemCard = (prop) => {
+const ItemCard = ({item}) => {
     
-    const image = require(`../assets/${prop.item.id}.jpg`);
+    const image = require(`../assets/${item.id}.jpg`);
 
     return(
         <div className="item-card">
-            <img src={image} alt={`item-${prop.item.id}`}/>
-            <h3 className="game-title">{prop.item.name}</h3>
-            <p className="game-price">{prop.item.price}</p>
+            <img src={image} alt={`item-${item.id}`}/>
+            <h3 className="game-title">{item.name}</h3>
+            <p className="game-price">{item.price}</p>
         </div>
     )
 }
