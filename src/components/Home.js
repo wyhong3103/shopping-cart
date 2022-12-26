2743d773d5c0485084f2c1da844ffbcb3adbafbc
wyhong3103 <wyhong3103@gmail.com>
2022-12-26 16:22:52 +0800

    Update subtitlte

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,7 +15,7 @@ const Home = () => {
             <div className="home-cont">
                 <div class="home-flex">
                     <h1>GamingWizard</h1>
-                    <p>We have everything you need here.</p>
+                    <p>We have everything you need.</p>
                 </div>
             </div>
         </div>
